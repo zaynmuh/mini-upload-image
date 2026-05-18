a79ac1aa6e9e9e33e4998eb01ff2e07c89269505
znveex <zaynmuhammad@MacBook-Zen.local>
2026-05-18 11:51:05 +0700

    upload image success

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,44 @@
-const fs = require("fs");
 const express = require("express");
-const multer = require("multer")
 const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
+// Storage setup
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 // Serve frontend files
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-// Homepage route
+// Serve uploaded images
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Upload route
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.json({
+    message: "Image uploaded successfully",
+    imageUrl: `/uploads/${req.file.filename}`,
+  });
 });
 
-// Get /image API
+// Get all uploaded images
 app.get("/images", (req, res) => {
   const uploadsPath = path.join(__dirname, "uploads");
 
@@ -33,4 +53,8 @@ app.get("/images", (req, res) => {
 
     res.json(imageUrls);
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
