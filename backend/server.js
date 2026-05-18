30b5d5dda85bc45fe572115d7e0da749453ba73e
znveex <zaynmuhammad@MacBook-Zen.local>
2026-05-18 11:19:56 +0700

    upload and show file

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+const fs = require("fs");
 const express = require("express");
+const multer = require("multer")
 const path = require("path");
 
 const app = express();
@@ -14,4 +16,21 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Get /image API
+app.get("/images", (req, res) => {
+  const uploadsPath = path.join(__dirname, "uploads");
+
+  fs.readdir(uploadsPath, (err, files) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Failed to load images",
+      });
+    }
+
+    const imageUrls = files.map((file) => `/uploads/${file}`);
+
+    res.json(imageUrls);
+  });
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,55 @@
+const uploadForm = document.getElementById("uploadForm");
+const imageInput = document.getElementById("imageInput");
+const feed = document.getElementById("feed");
 
-console.log("Frontend connected 🚀");
+// Load existing images
+async function loadImages() {
+  try {
+    const response = await fetch("/images");
+    const images = await response.json();
+
+    feed.innerHTML = "";
+
+    images.reverse().forEach((imageUrl) => {
+      const img = document.createElement("img");
+
+      img.src = imageUrl;
+      img.width = 300;
+
+      feed.appendChild(img);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Upload image
+uploadForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const file = imageInput.files[0];
+
+  if (!file) {
+    alert("Please select image");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("image", file);
+
+  try {
+    await fetch("/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    loadImages();
+
+    uploadForm.reset();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Initial load
+loadImages();
