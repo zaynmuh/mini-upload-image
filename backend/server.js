9c84728f633d4a3fc1bc2e6fba895d4eb1540a54
znveex <zaynmuhammad@MacBook-Zen.local>
2026-05-28 14:23:26 +0700

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const db = require("./database");
 
